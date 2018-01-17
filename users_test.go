25c9366d0e7bdd6c9337ac47bbb4989035b95adc
Lei Tian <taylor840326@gmail.com>
2018-01-17 11:37:48 +0800

    add users_test.go

---
+++ users_test.go
@@ -0,0 +1,27 @@
+package imSQL
+
+import (
+	"testing"
+)
+
+func TestFindAllUsers(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	conn.SetCharset("utf8")
+	conn.SetCollation("utf8_general_ci")
+	conn.MakeDBI()
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	allusers, err := FindAllUserInfo(db, 1, 0)
+	if err != nil {
+		t.Error(allusers, err)
+	}
+
+}
