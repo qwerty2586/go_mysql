d1408bfbde8d7b711e80d4fc44b7deeb74bc6e4a
Lei Tian <taylor840326@gmail.com>
2018-01-17 10:32:01 +0800

    add connections functions

---
+++ connections.go
@@ -0,0 +1,51 @@
+package imSQL
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/juju/errors"
+)
+
+type (
+	Conn struct {
+		Addr      string
+		Port      uint64
+		User      string
+		Password  string
+		Database  string
+		Charset   string
+		Collation string
+		DBI       string
+	}
+)
+
+func NewConn(addr string, port uint64, user string, password string) (*Conn, error) {
+
+}
+
+func (imsql *Conn) SetCharset(charset string) {
+	imsql.Charset = charset
+}
+
+func (imsql *Conn) SetCollation(collation string) {
+	imsql.Collation = collation
+}
+
+func (imsql *Conn) MakeDBI() {
+	imsq.DBI = fmt.Sprintf("%s:%s@tcp(%s:%d)g%s?charset=%s?collation=%s", imsql.User, imsql.Password, imsql.Addr, imsql.Port, imsql.Database, imsql.Charset, imsql.Collation)
+}
+
+func (imsql *Conn) OpenConn() (*sql.DB, error) {
+	db, err := sql.Open("mysql", imsql.DBI)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return db, nil
+}
