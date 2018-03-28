0f63e762a9dd5a71d711296eaa847905af42b6c6
Lei Tian <taylor840326@gmail.com>
2018-03-28 16:15:08 +0800

    add databases.go

---
+++ databases.go
@@ -0,0 +1,68 @@
+package imSQL
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/juju/errors"
+)
+
+type (
+	DB struct {
+		Name string `json:"name" db:"name"`
+	}
+)
+
+const (
+	/*Create one database*/
+	StmtCreateOneDatabase = `
+	CREATE DATABASE %s DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci
+	`
+
+	/*Drop a database.*/
+	StmtDropOneDatabase = `
+	DROP DATABASE %s	
+	`
+
+	StmtQueryUsersDB = `
+	SELECT Db FROM mysql.db WHERE User = '%s'
+	`
+)
+
+/*
+NewDB return a new db handler.
+*/
+func NewDB(name string) (*DB, error) {
+	newdb := new(DB)
+
+	newdb.Name = name
+
+	return newdb, nil
+}
+
+/*
+Create one database.
+*/
+func (dbi *DB) CreateOneDB(db *sql.DB) error {
+
+	Query := fmt.Sprintf(StmtCreateOneDatabase, dbi.Name)
+	_, err := db.Exec(Query)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	return nil
+}
+
+/*
+Drop one databases.
+*/
+func (dbi *DB) DropOneDB(db *sql.DB) error {
+
+	Query := fmt.Sprintf(StmtDropOneDatabase, dbi.Name)
+	_, err := db.Exec(Query)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	return nil
+}
