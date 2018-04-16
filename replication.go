10ce284ec831335465de5d098ba653be6792e05d
Lei Tian <taylor840326@gmail.com>
2018-04-16 14:39:52 +0800

    add NewSlave function.

--- replication.go
+++ replication.go
@@ -22,10 +22,10 @@ type (
 		MasterHeartbeatPeriod     uint64   `json:"master_heartbeat_period" db:"master_heartbeat_period"`
 		MasterLogFile             string   `json:"master_log_file" db:"master_log_file"`
 		MasterLogPos              uint64   `json:"master_log_pos" db:"master_log_pos"`
-		MasterAutoPosition        bool     `json:"master_auto_position" db:"master_auto_position"`
+		MasterAutoPosition        uint64   `json:"master_auto_position" db:"master_auto_position"`
 		RelayLogFile              string   `json:"relay_log_file" db:"relay_log_file"`
 		RelayLogPos               uint64   `json:"relay_log_pos" db:"relay_log_pos"`
-		MasterSSL                 bool     `json:"master_ssl" db:"master_ssl"`
+		MasterSSL                 uint64   `json:"master_ssl" db:"master_ssl"`
 		MasterSSLCa               string   `json:"master_ssl_ca" db:"master_ssl_ca"`
 		MasterSSLCapath           string   `json:"master_ssl_capath" db:"master_ssl_capath"`
 		MasterSSLCert             string   `json:"master_ssl_cert" db:"master_ssl_cert"`
@@ -33,8 +33,46 @@ type (
 		MasterSSLCrlpath          string   `json:"master_ssl_crlpath" db:"master_ssl_crlpath"`
 		MasterSSLKey              string   `json:"master_ssl_key" db:"master_ssl_key"`
 		MasterSSLCipher           string   `json:"master_ssl_cipher" db:"master_ssl_cipher"`
-		MasterSSLVerifyServerCert bool     `json:"master_ssl_verify_server_cert" db:"master_ssl_verify_server_cert"`
+		MasterSSLVerifyServerCert uint64   `json:"master_ssl_verify_server_cert" db:"master_ssl_verify_server_cert"`
 		MasterTLSVersion          string   `json:"master_tls_version" db:"master_tls_version"`
 		IgnoreServerIds           []string `json:"ignore_server_ids" db:"ignore_server_ids"`
 	}
 )
+
+// new slave struct.
+func NewSlave(host string, port uint64, username string, password string, filename string, filepos uint64) (*Slave, error) {
+
+	slave := new(Slave)
+
+	slave.MasterHost = host
+	slave.MasterPort = port
+	slave.MasterUser = username
+	slave.MasterPassword = password
+	slave.MasterLogFile = filename
+	slave.MasterLogPos = filepos
+
+	slave.MasterBind = ""
+	slave.MasterConnectRetry = 60
+	slave.MasterRetryCount = 86400
+	slave.MasterDelay = 0
+	slave.MasterHeartbeatPeriod = 0
+	slave.MasterAutoPosition = 0
+
+	slave.RelayLogFile = ""
+	slave.RelayLogPos = 0
+
+	slave.MasterSSL = 0
+	slave.MasterSSLCa = ""
+	slave.MasterSSLCapath = ""
+	slave.MasterSSLCapath = ""
+	slave.MasterSSLCert = ""
+	slave.MasterSSLCrl = ""
+	slave.MasterSSLCrlpath = ""
+	slave.MasterSSLKey = ""
+	slave.MasterSSLCipher = ""
+	slave.MasterSSLVerifyServerCert = 0
+	slave.MasterTLSVersion = ""
+	slave.IgnoreServerIds = []string{}
+
+	return slave, nil
+}
