7037329247f66948ed39290b82690d79db97b9fe
Lei Tian <taylor840326@gmail.com>
2018-04-16 14:13:41 +0800

    replication.go

---
+++ replication.go
@@ -0,0 +1,40 @@
+package imSQL
+
+type (
+	// show master status
+	Master struct {
+		File           string `json:"file" db:"file"`
+		Position       uint64 `json:"position" db:"position"`
+		BinlogDoDb     string `json:"binlog_do_db" db:"binlog_do_db"`
+		BinlogIgnoreDb string `json:"binlog_ignore_db" db:"binlog_ignore_db"`
+		ExecutedGTID   string `json:"executed_gtid_set" db:"executed_gtid_set"`
+	}
+	// change master to
+	Slave struct {
+		MasterBind                string   `json:"master_bind" db:"master_bind"`
+		MasterHost                string   `json:"master_host" db:"master_host"`
+		MasterUser                string   `json:"master_user" db:"master_user"`
+		MasterPassword            string   `json:"master_password" db:"master_password"`
+		MasterPort                uint64   `json:"master_port" db:"master_port"`
+		MasterConnectRetry        uint64   `json:"master_connect_retry" db:"master_connect_retry"`
+		MasterRetryCount          uint64   `json:"master_retry_count" db:"master_retry_count"`
+		MasterDelay               uint64   `json:"master_delay" db"master_delay"`
+		MasterHeartbeatPeriod     uint64   `json:"master_heartbeat_period" db:"master_heartbeat_period"`
+		MasterLogFile             string   `json:"master_log_file" db:"master_log_file"`
+		MasterLogPos              uint64   `json:"master_log_pos" db:"master_log_pos"`
+		MasterAutoPosition        bool     `json:"master_auto_position" db:"master_auto_position"`
+		RelayLogFile              string   `json:"relay_log_file" db:"relay_log_file"`
+		RelayLogPos               uint64   `json:"relay_log_pos" db:"relay_log_pos"`
+		MasterSSL                 bool     `json:"master_ssl" db:"master_ssl"`
+		MasterSSLCa               string   `json:"master_ssl_ca" db:"master_ssl_ca"`
+		MasterSSLCapath           string   `json:"master_ssl_capath" db:"master_ssl_capath"`
+		MasterSSLCert             string   `json:"master_ssl_cert" db:"master_ssl_cert"`
+		MasterSSLCrl              string   `json:"master_ssl_crl" db:"master_ssl_crl"`
+		MasterSSLCrlpath          string   `json:"master_ssl_crlpath" db:"master_ssl_crlpath"`
+		MasterSSLKey              string   `json:"master_ssl_key" db:"master_ssl_key"`
+		MasterSSLCipher           string   `json:"master_ssl_cipher" db:"master_ssl_cipher"`
+		MasterSSLVerifyServerCert bool     `json:"master_ssl_verify_server_cert" db:"master_ssl_verify_server_cert"`
+		MasterTLSVersion          string   `json:"master_tls_version" db:"master_tls_version"`
+		IgnoreServerIds           []string `json:"ignore_server_ids" db:"ignore_server_ids"`
+	}
+)
